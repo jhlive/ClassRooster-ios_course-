ee2b08aefee38ae63b6a024df633cbc16947bbc0
Rene Garcia <rene@generalui.com>
2015-02-19 19:09:17 -0800

    Adding person class

---
+++ ClassRoster/Person.swift
@@ -0,0 +1,17 @@
+//
+//  Person.swift
+//  ClassRoster
+//
+//  Created by René Alfredo García Hernández on 2/19/15.
+//  Copyright (c) 2015 ragar90. All rights reserved.
+//
+
+import Foundation
+class Person {
+    let firstName : String
+    let lastName : String
+    init(firstName : String, lastName : String, ){
+        self.firstName = firstName
+        self.lastName = lastName
+    }
+}
