684bcea1b66937f72b2315e620b07271d21cb4c0
Rene Garcia <rene@generalui.com>
2015-03-10 20:00:00 -0700

    Adding custom table cell view

---
+++ ClassRoster/PersonCell.swift
@@ -0,0 +1,26 @@
+//
+//  PersonCell.swift
+//  ClassRoster
+//
+//  Created by René Alfredo García Hernández on 3/10/15.
+//  Copyright (c) 2015 ragar90. All rights reserved.
+//
+
+import UIKit
+
+class PersonCell: UITableViewCell {
+
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var profileImageView: UIImageView!
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
